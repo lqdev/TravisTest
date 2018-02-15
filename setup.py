30619dddcdd40a688d1065ed6facaf112adfc0e2
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-15 20:30:14 +0000

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='testtravis',
+      version='0.0.1',
+      description='Package to get acquainted with travis functionality'
+      author='LQ',
+      url='https://github.com/lqdev/TravisTest',
+      packages=['travistest'],
+     )
