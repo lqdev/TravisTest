3799ee669a05e840ae82f0d64c9a3cbb117bf420
LQ <lqdev@outlook.com>
2018-02-15 18:26:07 -0500

    added setup.py back

---
+++ setup.py
@@ -0,0 +1,10 @@
+from disttools.core import setup
+
+setup(
+    name='travistest',
+    packages=['travistest'],
+    version='0.0.1',
+    description='Test project to get acquainted with TravisCI',
+    author='lqdev',
+    url='https://github.com/FaradayRF/faradayio',    
+)
