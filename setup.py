84c64cc5161c951469b10a0f9213ec9c14a9e75b
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-15 21:24:15 +0000

    updated travis deployment

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='travistest',
-      version='0.0.1',
-      description='Package to get acquainted with travis functionality'
-      author='LQ',
-      url='https://github.com/lqdev/TravisTest',
-      packages=['travistest'],
-     )
