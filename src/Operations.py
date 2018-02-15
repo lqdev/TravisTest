95d88a297775b9ce222b2fffbc9a32f6da9a5ea0
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-15 19:11:19 +0000

    added code and tests

---
+++ src/Operations.py
@@ -0,0 +1,12 @@
+class Operations:
+    def __init__(self):
+        pass
+    
+    def add(self,x,y):
+        return x + y
+
+    def subtract(self,x,y):
+        return x - y
+
+    def increment(self,x):
+        return x + 1;
---
+++ test/test_operations.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.fixture
+def op():
+    from src.Operations import Operations
+    return Operations()
+
+def test_add(op):
+    assert op.add(1,2) == 3
+
+def test_subtract(op):
+    assert op.subtract(2,1) == 1
+
+def test_increment(op):
+    assert op.increment(1) == 2
+    
