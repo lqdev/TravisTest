88492a004bbbe61ba56bfe33a5bc6faf5908efd4
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-15 21:11:55 +0000

    updated requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='testtravis',
+setup(name='travistest',
       version='0.0.1',
       description='Package to get acquainted with travis functionality'
       author='LQ',
