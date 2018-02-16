31d37b00837959a835631272d175df3d2d14f430
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-16 02:34:17 +0000

    using pymodules

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='travistest',
-    packages=['src'],
+    py_modules=['src/Operations.py'],
     version='0.0.1',
     description='Test project to get acquainted with TravisCI',
     author='lqdev',
