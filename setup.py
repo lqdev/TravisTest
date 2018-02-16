0b4e973b7064ce1bde38721f82c4f8891d731b7f
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-16 02:47:29 +0000

    removed author

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     packages=['travistest'],
     version='0.0.1',
     description='Test project to get acquainted with TravisCI',
-    author='lqdev',
     url='https://github.com/lqdev/TravisTest',    
 )
