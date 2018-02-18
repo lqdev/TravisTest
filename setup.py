8abc1792e1a2b85c9ea1e11a167977aa137645d2
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-18 14:24:51 +0000

    added coverage script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='travistest',
     packages=['travistest'],
-    version='0.0.4',
+    version='0.0.5',
     description='Test project to get acquainted with TravisCI',
     url='https://github.com/lqdev/TravisTest',    
 )
